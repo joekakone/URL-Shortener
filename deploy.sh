e9053b002844faef1a6572c9e051170a07d059ae
joekakone <joseph.kakone@gmail.com>
2024-06-22 21:18:57 +0000

    Update container name for demo

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/bash
 
 # Stop container
-docker stop mlsa-app-1
+docker stop mlsa-app-demo
 
 # Remove container
-docker rm mlsa-app-1
+docker rm mlsa-app-demo
 
 # Remove image
-docker image rm mlsa-app:v1
+docker image rm mlsa-app:demo
 
 # Build image
-docker build -f Dockerfile -t mlsa-app:v1 .
+docker build -f Dockerfile -t mlsa-app:demo .
 
 # Run container
 docker run -p 5000:5000 \
     --restart=always \
-    --name mlsa-app-1 \
-    -d mlsa-app:v1
+    --name mlsa-app-demo \
+    -d mlsa-app:demo
--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 # Tag
-docker image tag mlsa-app:v1 josephkonka.azurecr.io/mlsa-app:demo
+docker image tag mlsa-app:demo josephkonka.azurecr.io/mlsa-app:demo-v2
 
 # Push the image
-docker image push josephkonka.azurecr.io/mlsa-app:demo
+docker image push josephkonka.azurecr.io/mlsa-app:demo-v2
